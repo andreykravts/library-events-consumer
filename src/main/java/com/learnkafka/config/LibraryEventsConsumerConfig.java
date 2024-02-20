d73f7bd65791ad9791563ca8ed581e153de6915d
Andrey Kravtsov <63850989+andreykravts@users.noreply.github.com>
2024-02-20 09:20:00 +0200

    Update LibraryEventsConsumerConfig.java
    change consumer config file

--- src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
+++ src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
@@ -1,9 +1,28 @@
 package com.learnkafka.config;
 
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
 
 @Configuration
 @EnableKafka
 public class LibraryEventsConsumerConfig {
+      //with this factory we can create number consumers from our application
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<?,?> kafkaListenerContainerFactory(ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+                                                                               ConsumerFactory<Object,Object> kafkaConsumerFactory){
+        ConcurrentKafkaListenerContainerFactory<Object,Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+
+        configurer.configure(factory,kafkaConsumerFactory);
+        //only for manual acknowledge not a batch
+        //factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+        //set 3 listeners
+        //instances and each instance listen to one partition
+        factory.setConcurrency(3);
+        return factory;
+    }
 }
